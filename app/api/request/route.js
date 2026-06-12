e41e4473384058a6c53c7220bfcb7bf864080246
Mb4252 <118615883+Mb4252@users.noreply.github.com>
2026-06-12 14:27:21 +0200

    Update route.js

--- app/api/request/route.js
+++ app/api/request/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
-import { authOptions } from "../../auth/[...nextauth]/route"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
 import { supabaseAdmin } from "@/lib/supabase/admin"
 
 export async function POST(request) {
