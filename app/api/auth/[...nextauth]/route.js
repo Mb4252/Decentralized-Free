db272bcd3f65de9dde17ce52e6bc713628fc7a21
Mb4252 <118615883+Mb4252@users.noreply.github.com>
2026-06-12 13:55:59 +0200

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,5 @@
-import NextAuth from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
 import GoogleProvider from "next-auth/providers/google"
 import { supabaseAdmin } from "@/lib/supabase/admin"
 import bcrypt from "bcryptjs"
