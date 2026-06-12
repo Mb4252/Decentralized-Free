3ac8267ba93e6cef4dc2d94500bf82f09aedb8f6
Mb4252 <118615883+Mb4252@users.noreply.github.com>
2026-06-12 22:35:15 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  experimental: {
-    serverActions: true,
-  },
-  // منع ترجمة حزم Firebase
-  transpilePackages: ['firebase', '@firebase/auth', 'undici'],
+  // قم بإزالة experimental.serverActions
   webpack: (config, { isServer }) => {
-    // تجاهل بعض التحذيرات
-    config.ignoreWarnings = [{ module: /node_modules\/undici/ }];
-    
     if (!isServer) {
-      // لا تحل هذه الحزم على العميل
       config.resolve.fallback = {
-        ...config.resolve.fallback,
         fs: false,
         net: false,
         tls: false,
@@ -24,6 +12,6 @@ const nextConfig = {
     }
     return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
