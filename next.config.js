dde0d0b8c1710a6bf40ed048e7399bd2e57997f5
Mb4252 <118615883+Mb4252@users.noreply.github.com>
2026-06-13 07:33:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // قم بإزالة experimental.serverActions
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        tls: false,
-        undici: false,
-      };
-    }
-    return config;
-  },
-};
+  output: 'standalone',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
