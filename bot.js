d0cf1e1acd7766ce98296dd6423a1206d6bf08f8
Mb4252 <118615883+Mb4252@users.noreply.github.com>
2026-06-23 20:34:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ const API_KEY = process.env.BINGX_API_KEY;
 const API_SECRET = process.env.BINGX_API_SECRET;
 
 // ✅ إعدادات التداول
-const TRADE_AMOUNT = 1.2;
+const TRADE_AMOUNT = 0.0018;
 const USE_FULL_BALANCE = false;
 const MAX_RISK_PER_TRADE = 0.01;
 const LEVERAGE = 150;
